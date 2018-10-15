1c0f2c7c95bc98cf975c30e532d7344e5a7269e1
anbuKakashi <kakafuad12@gmail.com>
2018-10-15 16:22:49 +0700

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ env :PATH, ENV['PATH']
 MAILTO="meracle46@gmail.com
 
 every 1.minutes do 
-	command "/home/deployer/minicron-tes.sh >> /home/deployer/LOGNYOBA.txt # #{ENV['APPLICATION_ROOT']}"
+	command "/home/deployer/minicron-tes.sh >> /home/deployer/LOGNYOBA.txt #{ENV['APPLICATION_ROOT']}"
 end
